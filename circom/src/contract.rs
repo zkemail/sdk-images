7c5423c68efb2896da93389387fe045d88f605e4
Aditya Bisht <adityabisht64@gmail.com>
2024-11-20 23:19:04 +0530

    feat: rewrite circom image

---
+++ circom/src/contract.rs
@@ -0,0 +1,174 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+use regex::Regex;
+use relayer_utils::LOG;
+use sdk_utils::{run_command, run_command_and_return_output};
+use serde::Serialize;
+use slog::info;
+use tera::{Context, Tera};
+
+use crate::payload::Payload;
+
+#[derive(Serialize)]
+pub struct ContractData {
+    pub sender_domain: String,
+    pub values: Vec<Field>,
+    pub external_inputs: Vec<Field>,
+    pub signal_size: usize,
+}
+
+#[derive(Serialize)]
+pub struct Field {
+    pub name: String,
+    pub max_length: usize,
+    pub pack_size: usize,
+    pub start_idx: usize,
+}
+
+pub fn create_contract(contract_data: &ContractData) -> Result<()> {
+    // Initialize Tera
+    let mut tera = Tera::default();
+    tera.add_template_file("./template/contract.sol.tera", Some("contract.sol"))?;
+
+    let mut context = Context::new();
+    context.insert("sender_domain", &contract_data.sender_domain);
+    context.insert("values", &contract_data.values);
+    context.insert("external_inputs", &contract_data.external_inputs);
+    context.insert("signal_size", &contract_data.signal_size);
+
+    let rendered_contract = tera.render("contract.sol", &context)?;
+
+    // Write the rendered template to a file
+    std::fs::write("src/contract.sol", rendered_contract)?;
+
+    Ok(())
+}
+
+pub fn prepare_contract_data(payload: &Payload) -> ContractData {
+    let mut signal_size = 1; // Start with 1 as per your logic
+    let mut current_idx = 1; // Start index for the first field
+
+    let mut values = Vec::new();
+    for regex in payload.blueprint.decomposed_regexes.iter() {
+        let pack_size = ((regex.max_length as f64) / 31.0).ceil() as usize;
+        let field = Field {
+            name: regex.name.clone(),
+            max_length: regex.max_length,
+            pack_size,
+            start_idx: current_idx,
+        };
+        signal_size += pack_size;
+        current_idx += pack_size;
+        values.push(field);
+    }
+
+    let mut external_inputs = Vec::new();
+    if let Some(inputs) = &payload.blueprint.external_inputs {
+        for input in inputs.iter() {
+            let pack_size = ((input.max_length as f64) / 31.0).ceil() as usize;
+            let field = Field {
+                name: input.name.clone(),
+                max_length: input.max_length,
+                pack_size,
+                start_idx: current_idx,
+            };
+            signal_size += pack_size;
+            current_idx += pack_size;
+            external_inputs.push(field);
+        }
+    }
+
+    let sender_domain = payload
+        .blueprint
+        .sender_domain
+        .clone()
+        .expect("Sender domain not found");
+
+    ContractData {
+        sender_domain,
+        values,
+        external_inputs,
+        signal_size,
+    }
+}
+
+pub async fn generate_verifier_contract(tmp_dir: &str) -> Result<()> {
+    // Unzip circuit files into the tmps folder
+    info!(LOG, "Unzipping circuit");
+    run_command("unzip", &["-o", "keys.zip"], Some(tmp_dir)).await?;
+
+    // Generate the verifier contract
+    info!(LOG, "Generating verifier contract");
+    run_command(
+        "snarkjs",
+        &[
+            "zkey",
+            "export",
+            "solidityverifier",
+            "circuit.zkey",
+            "verifier.sol",
+        ],
+        Some(tmp_dir),
+    )
+    .await?;
+
+    // Copy the verifier contract to the src folder
+    info!(LOG, "Copying verifier contract");
+    run_command(
+        "cp",
+        &["verifier.sol", "../src/verifier.sol"],
+        Some(tmp_dir),
+    )
+    .await?;
+
+    Ok(())
+}
+
+pub async fn deploy_verifier_contract(payload: Payload) -> Result<String> {
+    info!(LOG, "Building contracts");
+    run_command("yarn", &["build"], None).await?;
+
+    info!(LOG, "Deploying contracts");
+    let output = run_command_and_return_output("yarn", &["deploy"], None).await?;
+
+    // Parse the output to extract addresses
+    let re = Regex::new(r"Deployed (Groth16Verifier|Contract|DKIMRegistry) at (0x[a-fA-F0-9]{40})")
+        .unwrap();
+    let mut contract_addresses = HashMap::new();
+    for cap in re.captures_iter(&output) {
+        let contract_name = &cap[1];
+        let address = &cap[2];
+        contract_addresses.insert(contract_name.to_string(), address.to_string());
+        info!(LOG, "Deployed {} at address: {}", contract_name, address);
+    }
+
+    info!(LOG, "Verify contracts");
+    run_command(
+        "forge",
+        &[
+            "verify-contract",
+            "--chain-id",
+            payload.chain_id.to_string().as_str(),
+            contract_addresses.get("Groth16Verifier").unwrap(),
+            "src/verifier.sol:Groth16Verifier",
+        ],
+        None,
+    )
+    .await?;
+
+    run_command(
+        "forge",
+        &[
+            "verify-contract",
+            "--chain-id",
+            payload.chain_id.to_string().as_str(),
+            contract_addresses.get("Contract").unwrap(),
+            "src/contract.sol:Contract",
+        ],
+        None,
+    )
+    .await?;
+
+    Ok(contract_addresses.get("Contract").unwrap().to_string())
+}
---
+++ circom/src/main.rs
@@ -0,0 +1,199 @@
+mod contract;
+mod db;
+mod payload;
+mod template;
+
+use std::cmp::max;
+
+use anyhow::Result;
+use rand::Rng;
+use relayer_utils::LOG;
+use sdk_utils::{run_command, run_command_and_return_output, run_command_with_input};
+use slog::info;
+use sqlx::postgres::PgPoolOptions;
+use template::{generate_circuit, generate_regex_circuits, CircuitTemplateInputs};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let payload = payload::load_payload()?;
+    info!(LOG, "Loaded configuration: {:?}", payload);
+
+    // let pool = PgPoolOptions::new()
+    //     .max_connections(10)
+    //     .connect(&payload.database_url)
+    //     .await?;
+    // println!("Database connection established");
+
+    let blueprint = payload.blueprint;
+    let upload_url = payload.upload_url;
+
+    setup().await?;
+
+    generate_regex_circuits(blueprint.clone().decomposed_regexes)?;
+
+    let circuit_template_inputs = CircuitTemplateInputs::from(blueprint);
+
+    let circuit = generate_circuit(circuit_template_inputs)?;
+
+    // Write the circuit to a file
+    let circuit_path = "./tmp/circuit.circom";
+    std::fs::write(circuit_path, circuit)?;
+
+    let ptau: usize = compile_circuit("tmp/circuit.zip").await?;
+
+    println!("ptau: {}", ptau);
+
+    // generate_keys("tmp", ptau).await?;
+
+    Ok(())
+}
+
+async fn setup() -> Result<()> {
+    // Remove and recreate tmp and regex directories
+    if std::path::Path::new("./tmp").exists() {
+        std::fs::remove_dir_all("./tmp")?;
+    }
+    std::fs::create_dir_all("./tmp")?;
+
+    // Create regex directory inside tmp
+    if std::path::Path::new("./tmp/regex").exists() {
+        std::fs::remove_dir_all("./tmp/regex")?;
+    }
+    std::fs::create_dir_all("./tmp/regex")?;
+
+    Ok(())
+}
+
+async fn compile_circuit(circuit_path: &str) -> Result<usize> {
+    // Run yarn install in the tmp folder
+    info!(LOG, "Running yarn install");
+    run_command("yarn", &[], Some("tmp")).await?;
+
+    // Compile the circuit
+    info!(LOG, "Compiling circuit");
+    let compile_result = run_command_and_return_output(
+        "circom",
+        &[
+            "circuit.circom",
+            "--sym",
+            "--r1cs",
+            "--c",
+            "--wasm",
+            "-l",
+            "../node_modules",
+        ],
+        Some("tmp"),
+    )
+    .await?;
+
+    // Find the number of non-linear constraints to determine the power of tau
+    // Initialize variables for constraints and wires
+    let mut constraints = 0u64;
+    let mut wires = 0u64;
+
+    // Iterate over each line in the input string
+    for line in compile_result.lines() {
+        if line.starts_with("non-linear constraints:") {
+            // Extract the number of constraints
+            if let Some(value_str) = line.split(':').nth(1) {
+                constraints = value_str.trim().parse::<u64>().unwrap_or(0);
+            }
+        } else if line.starts_with("wires:") {
+            // Extract the number of wires
+            if let Some(value_str) = line.split(':').nth(1) {
+                wires = value_str.trim().parse::<u64>().unwrap_or(0);
+            }
+        }
+    }
+
+    // Determine the maximum value between constraints and wires
+    let max_value = max(constraints, wires);
+
+    // Compute the minimal power k such that 2^k > max_value
+    let mut k = 0u32;
+    while (1u64 << k) <= max_value {
+        k += 1;
+    }
+
+    println!("k: {}", k);
+
+    // Run make in the circuit_cpp folder
+    info!(LOG, "Compiling circuit");
+    run_command(
+        "circuit-compile",
+        &["--cpp", "circuit.cpp"],
+        Some("tmp/circuit_cpp"),
+    )
+    .await?;
+
+    Ok(k as usize)
+}
+
+async fn generate_keys(tmp_dir: &str, ptau: usize) -> Result<()> {
+    // Generate the proving and verification keys
+    info!(LOG, "Downloading ptau file");
+    run_command(
+        "curl",
+        &[
+            "-o",
+            "pot_final.ptau",
+            format!(
+                "https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_{}.ptau",
+                ptau
+            )
+            .as_str(),
+        ],
+        Some(tmp_dir),
+    )
+    .await?;
+
+    // Generate zkey
+    info!(LOG, "Generating zkey");
+    run_command(
+        "snarkjs",
+        &[
+            "groth16",
+            "setup",
+            "circuit.r1cs",
+            "pot_final.ptau",
+            "circuit_0000.zkey",
+        ],
+        Some(tmp_dir),
+    )
+    .await?;
+
+    // Contribute to zkey
+    info!(LOG, "Contributing to zkey");
+    let random_input: u32 = rand::thread_rng().gen_range(100..1000);
+    let random_input_str = format!("{}\n", random_input);
+    run_command_with_input(
+        "snarkjs",
+        &[
+            "zkey",
+            "contribute",
+            "circuit_0000.zkey",
+            "circuit.zkey",
+            "-v",
+        ],
+        Some(tmp_dir),
+        &random_input_str,
+    )
+    .await?;
+
+    // Export verification key
+    info!(LOG, "Exporting verification key");
+    run_command(
+        "snarkjs",
+        &[
+            "zkey",
+            "export",
+            "verificationkey",
+            "circuit.zkey",
+            "verification_key.json",
+        ],
+        Some(tmp_dir),
+    )
+    .await?;
+
+    Ok(())
+}
