d51115e61067645113ac44b5f784e5f2bff67fa8
Bence Haromi <bence.haromi@gmail.com>
2025-03-20 09:41:56 +0100

    feat: small VerifierRouter changes

--- circom/contracts/VerifierRouter.sol
+++ circom/contracts/VerifierRouter.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.26;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
@@ -14,9 +14,9 @@ struct VerifierInfo {
 /// @notice Routes verification requests to appropriate registered verifier contracts
 /// @dev Acts as a central registry and gateway for multiple ZK verification contracts
 contract VerifierRouter is Ownable {
-    error InvalidVerifierAddress();
-    error VerifierAlreadyRegistered();
-    error VerifierNotRegistered();
+    error InvalidVerifierAddress(address verifier);
+    error VerifierAlreadyRegistered(uint256 verifierId);
+    error VerifierNotRegistered(uint256 verifierId);
     error VerificationFailed();
 
     event VerifierRegistered(uint256 indexed id, address indexed verifier, bytes4 functionSelector);
@@ -29,28 +29,6 @@ contract VerifierRouter is Ownable {
 
     constructor(address initialOwner) Ownable(initialOwner) {}
 
-    /// @notice Verifies a proof using the specified verifier
-    /// @param verifierId The ID of the verifier to use
-    /// @param a First part of the ZK proof
-    /// @param b Second part of the ZK proof
-    /// @param c Third part of the ZK proof
-    /// @param publicOutputs Public inputs to the ZK circuit
-    function verify(
-        uint256 verifierId,
-        uint256[2] calldata a,
-        uint256[2][2] calldata b,
-        uint256[2] calldata c,
-        uint256[] calldata publicOutputs
-    ) external view {
-        VerifierInfo memory info = verifierRegistry[verifierId];
-        if (info.verifierAddress == address(0)) revert VerifierNotRegistered();
-
-        bytes memory data = abi.encodePacked(info.functionSelector, a, b, c, publicOutputs);
-
-        (bool success,) = info.verifierAddress.staticcall(data);
-        if (!success) revert VerificationFailed();
-    }
-
     /// @notice Registers a new verifier contract
     /// @param verifierId Unique identifier for the verifier
     /// @param verifierAddress Address of the verifier contract
@@ -59,8 +37,10 @@ contract VerifierRouter is Ownable {
         external
         onlyOwner
     {
-        if (verifierAddress == address(0)) revert InvalidVerifierAddress();
-        if (verifierRegistry[verifierId].verifierAddress != address(0)) revert VerifierAlreadyRegistered();
+        if (verifierAddress == address(0)) revert InvalidVerifierAddress(verifierAddress);
+        if (verifierRegistry[verifierId].verifierAddress != address(0)) {
+            revert VerifierAlreadyRegistered(verifierId);
+        }
 
         verifierRegistry[verifierId] = VerifierInfo(verifierAddress, functionSelector);
         emit VerifierRegistered(verifierId, verifierAddress, functionSelector);
@@ -71,8 +51,7 @@ contract VerifierRouter is Ownable {
     /// @param verifierAddress New address of the verifier contract
     /// @param functionSelector New function selector for the verification method
     function updateVerifier(uint256 verifierId, address verifierAddress, bytes4 functionSelector) external onlyOwner {
-        if (verifierAddress == address(0)) revert InvalidVerifierAddress();
-        if (verifierRegistry[verifierId].verifierAddress == address(0)) revert VerifierNotRegistered();
+        if (verifierRegistry[verifierId].verifierAddress == address(0)) revert VerifierNotRegistered(verifierId);
 
         address oldVerifier = verifierRegistry[verifierId].verifierAddress;
         verifierRegistry[verifierId] = VerifierInfo(verifierAddress, functionSelector);
@@ -82,7 +61,7 @@ contract VerifierRouter is Ownable {
     /// @notice Unregisters a verifier
     /// @param verifierId Identifier of the verifier to remove
     function unregisterVerifier(uint256 verifierId) external onlyOwner {
-        if (verifierRegistry[verifierId].verifierAddress == address(0)) revert VerifierNotRegistered();
+        if (verifierRegistry[verifierId].verifierAddress == address(0)) revert VerifierNotRegistered(verifierId);
         // Unchecked block to save gas since there's no risk of underflow/overflow when deleting
         unchecked {
             delete verifierRegistry[verifierId];
@@ -101,6 +80,30 @@ contract VerifierRouter is Ownable {
     /// @param verifierId The ID of the verifier.
     /// @return The verifier information.
     function getVerifierInfo(uint256 verifierId) external view returns (VerifierInfo memory) {
+        if (verifierRegistry[verifierId].verifierAddress == address(0)) revert VerifierNotRegistered(verifierId);
+
         return verifierRegistry[verifierId];
+    }
+
+    /// @notice Verifies a proof using the specified verifier
+    /// @param verifierId The ID of the verifier to use
+    /// @param a First part of the ZK proof
+    /// @param b Second part of the ZK proof
+    /// @param c Third part of the ZK proof
+    /// @param publicOutputs Public inputs to the ZK circuit
+    function verify(
+        uint256 verifierId,
+        uint256[2] calldata a,
+        uint256[2][2] calldata b,
+        uint256[2] calldata c,
+        uint256[] calldata publicOutputs
+    ) external view {
+        VerifierInfo memory info = verifierRegistry[verifierId];
+        if (info.verifierAddress == address(0)) revert VerifierNotRegistered(verifierId);
+
+        bytes memory data = abi.encodePacked(info.functionSelector, a, b, c, publicOutputs);
+
+        (bool success,) = info.verifierAddress.staticcall(data);
+        if (!success) revert VerificationFailed();
     }
 }
