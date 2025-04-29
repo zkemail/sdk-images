e5b989e61394b7215d1a0586ce7b1354833d535c
Dimitri Dumonet <dimitridumonet@gmail.com>
2025-04-29 13:23:12 +0200

    feat: add api key auth

--- noir/src/main.rs
+++ noir/src/main.rs
@@ -1,12 +1,51 @@
 use anyhow::Result;
-use axum::{Router, routing::post};
+use axum::{Router, middleware, routing::post};
+use dotenv::dotenv;
+use relayer_utils::LOG;
+use slog::warn;
 
 use noir::handlers::compile_circuit_handler;
 
+#[derive(Clone)]
+struct AppState {
+    api_key: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
     println!("Starting server");
-    let app = Router::new().route("/compile", post(compile_circuit_handler));
+
+    let state = AppState {
+        api_key: std::env::var("ZKEMAIL_API_KEY").expect("ZKEMAIL_API_KEY must be set"),
+    };
+
+    // Middleware to check API key
+    async fn auth_middleware(
+        state: axum::extract::State<AppState>,
+        req: axum::extract::Request,
+        next: middleware::Next,
+    ) -> Result<axum::response::Response, axum::http::StatusCode> {
+        let query = req.uri().query().unwrap_or("");
+
+        if query
+            .split('&')
+            .any(|param| param == format!("api_key={}", state.api_key))
+        {
+            Ok(next.run(req).await)
+        } else {
+            warn!(LOG, "Unauthorized request attempt");
+            Err(axum::http::StatusCode::UNAUTHORIZED)
+        }
+    }
+
+    let app = Router::new()
+        .route("/compile", post(compile_circuit_handler))
+        .layer(middleware::from_fn_with_state(
+            state.clone(),
+            auth_middleware,
+        ))
+        .with_state(state);
 
     // Get port from environment variable or use default
     let port = std::env::var("PORT").unwrap_or_else(|_| "8082".to_string());
