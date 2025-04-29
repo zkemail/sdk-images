807dcc9529f974250ecc504a24e7a966eb20ca1a
Dimitri Dumonet <dimitridumonet@gmail.com>
2025-04-29 12:22:04 +0200

    chore: Take port from env

--- noir/src/main.rs
+++ noir/src/main.rs
@@ -8,7 +8,12 @@ async fn main() -> Result<()> {
     println!("Starting server");
     let app = Router::new().route("/compile", post(compile_circuit_handler));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8082").await.unwrap();
+    // Get port from environment variable or use default
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8082".to_string());
+
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .unwrap();
     axum::serve(listener, app).await.unwrap();
 
     Ok(())
