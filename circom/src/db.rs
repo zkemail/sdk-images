0478a83a59cdf89dcbd7e86ae9eba20ea0790c2a
benceharomi <56651250+benceharomi@users.noreply.github.com>
2025-05-02 14:54:50 +0100

    chore: circom general cleanup (#13)
    * chore: unused manifest keys removed
    
    * chore: comit to rev in Cargo.toml
    
    * chore: clippy
    
    * chore: build warnings solved

--- circom/src/db.rs
+++ circom/src/db.rs
@@ -21,18 +21,18 @@ pub async fn update_verifier_contract_address(
     Ok(())
 }
 
-pub async fn update_ptau(pool: &Pool<Postgres>, id: Uuid, ptau: usize) -> Result<()> {
-    let query = r#"
-        UPDATE blueprints
-        SET ptau = $1
-        WHERE id = $2
-    "#;
+// pub async fn update_ptau(pool: &Pool<Postgres>, id: Uuid, ptau: usize) -> Result<()> {
+//     let query = r#"
+//         UPDATE blueprints
+//         SET ptau = $1
+//         WHERE id = $2
+//     "#;
 
-    sqlx::query(query)
-        .bind(ptau as i32)
-        .bind(id)
-        .execute(pool)
-        .await?;
+//     sqlx::query(query)
+//         .bind(ptau as i32)
+//         .bind(id)
+//         .execute(pool)
+//         .await?;
 
-    Ok(())
-}
+//     Ok(())
+// }
