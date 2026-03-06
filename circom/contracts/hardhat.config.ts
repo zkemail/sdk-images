3935d9c9a65570d51c049cd214f61c2de4a518d6
Bence Háromi <56651250+benceharomi@users.noreply.github.com>
2026-03-06 17:44:22 +0000

    fix: address P2: Most of these devDependencies are unused

--- circom/contracts/hardhat.config.ts
+++ circom/contracts/hardhat.config.ts
@@ -1,5 +1,6 @@
 import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ethers";
+import "@nomicfoundation/hardhat-verify";
 import "@parity/hardhat-polkadot";
 import "dotenv/config";
 
